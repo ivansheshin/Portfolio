9df89c2301de9aa8cbe8c741366e193e7372f492
ivansheshin <ivansheshin@gmail.com>
2021-06-04 22:40:29 +0300

    Меню для мобильной версии

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,24 @@
-function initListener(){
+function initListener() {
     const menuBtn = document.querySelector('.header-button');
     const navList = document.querySelector('.header-nav-list');
 
-    menuBtn.addEventListener('click', function(){
+    if(window.matchMedia('(max-width: 480px)').matches){
+        navList.insertAdjacentHTML("beforeend", '<li class="header-nav-list__item_end">Exit Menu</li>');
+    }
+
+    menuBtn.addEventListener('click', function () {
         navList.classList.add('open');
         menuBtn.classList.add('close');
+
     })
+    const navListClose = document.querySelector('.header-nav-list__item_end')
+
+
+    navListClose.addEventListener('click', function () {
+        navList.classList.remove('open');
+        menuBtn.classList.remove('close');
+    })
+
+
 }
 initListener();
