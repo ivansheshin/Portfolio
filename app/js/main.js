e3d5b9f285711505c9cd3ea0ef30830c9995a9ff
ivansheshin <ivansheshin@gmail.com>
2021-06-05 17:52:45 +0300

    123

--- app/js/main.js
+++ app/js/main.js
@@ -2,19 +2,17 @@ function initListener() {
     const menuBtn = document.querySelector('.header-button');
     const navList = document.querySelector('.header-nav-list');
 
-
-
-
     menuOpen(menuBtn, navList);
 
     const navListClose = document.querySelector('.header-nav-list__link_end')
-    menuClose (navListClose, navList, menuBtn);
+
+    menuClose(navListClose, navList, menuBtn);
 
 
 }
 
 
-function menuOpen(listener, list){
+function menuOpen(listener, list) {
     listener.addEventListener('click', function () {
         list.classList.add('open');
         listener.classList.add('close');
@@ -22,11 +20,10 @@ function menuOpen(listener, list){
     })
 }
 
-function menuClose(listener, list, btn){
-   listener.addEventListener('click', function(){
+function menuClose(listener, list, btn) {
+    listener.addEventListener('click', function () {
         list.classList.remove('open');
         btn.classList.remove('close');
     })
 }
 initListener();
-
