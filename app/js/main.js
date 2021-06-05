9401b026108c41236f739639378deec936f3d6da
ivansheshin <ivansheshin@gmail.com>
2021-06-05 18:31:03 +0300

    123

--- app/js/main.js
+++ app/js/main.js
@@ -2,27 +2,27 @@ function initListener() {
     const menuBtn = document.querySelector('.header-button');
     const navList = document.querySelector('.header-nav-list');
 
-    menuOpen(menuBtn, navList);
+    openMenu(menuBtn, navList);
 
     const navListClose = document.querySelector('.header-nav-list__link_end')
 
-    menuClose(navListClose, navList, menuBtn);
+    closeMenu(navListClose, navList, menuBtn);
 
 
 }
 
 
-function menuOpen(listener, list) {
-    listener.addEventListener('click', function () {
-        list.classList.add('open');
+function openMenu(element, menu) {
+    element.addEventListener('click', function () {
+        menu.classList.add('open');
         listener.classList.add('close');
 
     })
 }
 
-function menuClose(listener, list, btn) {
-    listener.addEventListener('click', function () {
-        list.classList.remove('open');
+function closeMenu(element, menu, btn) {
+    element.addEventListener('click', function () {
+        menu.classList.remove('open');
         btn.classList.remove('close');
     })
 }
